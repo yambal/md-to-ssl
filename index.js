9badfac156fa5af4bd8148bf6923a4841b89897c
June YAMAMOTO <june@yambal.net>
2020-02-15 10:18:02 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var marked = __importStar(require("marked"));
+/**
+ * https://marked.js.org/#/USING_PRO.md#renderer
+ * @param markdown https://marked.js.org/#/USING_PRO.md#renderer
+ * @param title
+ * @param description
+ */
+exports.mdToSsml = function (markdown, title, description) {
+    var renderer = new marked.Renderer();
+    renderer.heading = function (text, level, raw, slug) {
+        // console.log(slug)
+        return "<par>\n  <media>\n    <emphasis level=\"strong\">\n    " + text + "\n    </emphasis>\n  </media>\n</par>\n<break time=\"1.5s\" />\n";
+    };
+    // Blockquote
+    renderer.blockquote = function (text) {
+        return "<p><prosody rate=\"slow\">" + text + "</prosody></p><break time=\"2s\" />\n";
+    };
+    // p
+    renderer.paragraph = function (text) {
+        return "<p>" + text + "</p><break time=\"2s\" />\n";
+    };
+    // hr
+    renderer.hr = function () {
+        return "<break time=\"3s\" />\n";
+    };
+    // list
+    renderer.list = function (body, ordered, start) {
+        return "<p>" + body + "</p>";
+    };
+    renderer.listitem = function (text) {
+        return "<p>" + text + "</p>";
+    };
+    // Strong
+    renderer.strong = function (text) {
+        return "<break time=\"0.25s\" /><emphasis level=\"strong\">" + text + "</emphasis><break time=\"0.25s\" />";
+    };
+    // BR
+    renderer.br = function () {
+        return "<break time=\"1s\" />\n";
+    };
+    // @ts-ignore: Unreachable code error
+    var parsed = marked(markdown, { renderer: renderer });
+    var openning = "<emphasis level=\"strong\">\n  <prosody rate=\"slow\" pitch=\"+0.12st\">" + title + "</prosody>\n</emphasis>\n<break time=\"2s\" />\n<p>" + description + "</p><break time=\"2s\" />\n";
+    return "<speak><prosody rate=\"125%\">" + openning + parsed + "</prosody></speak>";
+};
+// <audio src="https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg"></audio>
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import * as marked from 'marked'
+
+/**
+ * https://marked.js.org/#/USING_PRO.md#renderer
+ * @param markdown https://marked.js.org/#/USING_PRO.md#renderer
+ * @param title 
+ * @param description 
+ */
+
+export const mdToSsml = (markdown: string, title?: string, description?: string) => {
+  const renderer = new marked.Renderer()
+
+  renderer.heading = (text: string, level: number, raw: string, slug: any) => {
+    // console.log(slug)
+    return `<par>
+  <media>
+    <emphasis level="strong">
+    ${text}
+    </emphasis>
+  </media>
+</par>
+<break time="1.5s" />
+`};
+
+  // Blockquote
+  renderer.blockquote = (text: string) => {
+    return `<p><prosody rate="slow">${text}</prosody></p><break time="2s" />\n`}
+
+  // p
+  renderer.paragraph = (text: string) => {
+    return `<p>${text}</p><break time="2s" />\n`}
+
+  // hr
+  renderer.hr= () => {
+    return `<break time="3s" />\n`}
+
+  // list
+  renderer.list = (body: string, ordered: boolean, start: number) => {
+    return `<p>${body}</p>`}
+  renderer.listitem = (text: string) => {
+    return `<p>${text}</p>`}
+
+  // Strong
+  renderer.strong = function (text: string) {
+    return `<break time="0.25s" /><emphasis level="strong">${text}</emphasis><break time="0.25s" />`
+  };
+
+  // BR
+  renderer.br = function () {
+    return `<break time="1s" />\n`
+  };
+
+  // @ts-ignore: Unreachable code error
+  const parsed = marked(markdown, { renderer: renderer })
+
+  const openning = `<emphasis level="strong">
+  <prosody rate="slow" pitch="+0.12st">${title}</prosody>
+</emphasis>
+<break time="2s" />
+<p>${description}</p><break time="2s" />\n`
+
+  return `<speak><prosody rate="125%">${openning}${parsed}</prosody></speak>`
+}
+
+// <audio src="https://actions.google.com/sounds/v1/animals/cat_purr_close.ogg"></audio>
